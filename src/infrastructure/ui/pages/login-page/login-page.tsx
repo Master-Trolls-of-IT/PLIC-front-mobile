92956193c9270c2ef62855d9900e27c1eff0b156
Mathis Moreau <mathis.moreau@epita.fr>
2023-03-29 11:56:19 +0200

    Add loader for startup page

---
+++ src/infrastructure/ui/pages/login-page/login-page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const LoginPage = () => {
+    return (
+        <View>
+            <Text>
+                coucou les meufs
+            </Text>
+        </View>
+    )
+}
+
+export default LoginPage;
