5cbc2e07128e6c2f87dc9bc70a465732bd1ed643
Maxner Demilon <124256635+AymanImhah@users.noreply.github.com>
2023-05-02 13:10:24 +0200

    Update generic-input-birthdate.tsx

--- src/infrastructure/ui/shared/component/inputs/generic-input-birthdate/generic-input-birthdate.tsx
+++ src/infrastructure/ui/shared/component/inputs/generic-input-birthdate/generic-input-birthdate.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import GenericInput from '~/infrastructure/ui/shared/component/inputs/generic-input/generic-input';
 import { GenericInputBirthdateProps } from '~/domain/interfaces/props/generic-input-birthdate-props';
-<<<<<<<< HEAD:src/infrastructure/ui/shared/component/inputs/generic-input-birthdate/generic-input-birthdate.tsx
+
 import { InputEnum } from '~/domain/interfaces/enum/input-type-enum';
-import useGenericInputBirthdateData from '~/infrastructure/ui/shared/component/inputs/generic-input-birthdate/hooks';
-========
-import { InputTypeEnum } from '~/domain/interfaces/enum/input-type-enum';
 import useGenericInputBirthdateData from '~/infrastructure/ui/shared/component/inputs/generique-input-birthdate/hooks';
->>>>>>>> a0e52729b6d268d26cf1655eab2d152c13ebaa74:src/infrastructure/ui/shared/component/inputs/generique-input-birthdate/generic-input-birthdate.tsx
 
 const GenericInputBirthdate = ({ title, input, dispatch, style, placeHolder }: GenericInputBirthdateProps) => {
     const { onChange } = useGenericInputBirthdateData(dispatch, input);
