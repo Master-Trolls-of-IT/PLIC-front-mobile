79398af0b6f74a76099f92e371c2f1d96ad18bb1
AkaMirai9 <105291924+AkaMirai9@users.noreply.github.com>
2023-05-03 16:23:27 +0200

    restore App.tsx

--- src/infrastructure/ui/App.tsx
+++ src/infrastructure/ui/App.tsx
@@ -1,68 +1,63 @@
 import React from 'react';
 import { registerRootComponent } from 'expo';
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import useAppData from '~/infrastructure/ui/hooks';
-import SmallMultipleIntakes from '~/infrastructure/ui/shared/component/widgets/my-intakes/small-multiple/small-multiple-intakes';
-import { NutrientsEnum } from '~/domain/interfaces/enum/nutrients-enum';
-import LargeIntakes from '~/infrastructure/ui/shared/component/widgets/my-intakes/large/large-intakes';
-
+import AppContainer from '~/infrastructure/ui/shared/component/app-container/app-container';
+import { StoreProvider } from '~/infrastructure/controllers/store';
+import { PagesEnum } from '~/domain/interfaces/enum/pages-enum';
+import StartUpPage from '~/infrastructure/ui/pages/startup-page/startup-page';
+import LoginPage from '~/infrastructure/ui/pages/login-page/login-page';
+import SignUpPage from '~/infrastructure/ui/pages/sign-up-page/sign-up-page';
+import MealPage from '~/infrastructure/ui/pages/meal-page/meal-page';
+import ScanPage from '~/infrastructure/ui/pages/scan-page/scan-page';
+import HomePage from '~/infrastructure/ui/pages/home-page/home-page';
+import RecipePage from '~/infrastructure/ui/pages/recipes-page/recipe-page';
+import GamePage from '~/infrastructure/ui/pages/game-page/game-page';
+import HistoricalPage from '~/infrastructure/ui/pages/historical-page/historical-page';
 function App() {
     const { rootStore, Stack, gestureEnabled, gestureDisabled } = useAppData();
 
     return (
-        <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 250 }}>
-            <LargeIntakes
-                energy={{ nutrientType: NutrientsEnum.Energy, goal: 2050, earned: 1620 }}
-                firstNutrient={{ nutrientType: NutrientsEnum.Protein, goal: 79, earned: 62 }}
-                secondNutrient={{ nutrientType: NutrientsEnum.Carbohydrate, goal: 53, earned: 12 }}
-                thirdNutrient={{ nutrientType: NutrientsEnum.Fiber, goal: 23, earned: 15 }}
-            />
-            <SmallMultipleIntakes
-                firstNutrient={{ nutrientType: NutrientsEnum.Protein, goal: 79, earned: 62 }}
-                secondNutrient={{ nutrientType: NutrientsEnum.Carbohydrate, goal: 53, earned: 12 }}
-                thirdNutrient={{ nutrientType: NutrientsEnum.Fiber, goal: 23, earned: 15 }}
-            />
-        </View>
-        // <StoreProvider value={rootStore}>
-        //     <AppContainer>
-        //         <NavigationContainer>
-        //             <Stack.Navigator
-        //                 initialRouteName={PagesEnum.StartUpPage}
-        //                 screenOptions={{
-        //                     headerShown: false
-        //                 }}>
-        //                 {/* Authentification group */}
-        //                 <Stack.Group>
-        //                     <Stack.Screen
-        //                         name={PagesEnum.StartUpPage}
-        //                         component={StartUpPage}
-        //                         options={gestureDisabled}
-        //                     />
-        //                     <Stack.Screen name={PagesEnum.LoginPage} component={LoginPage} options={gestureDisabled} />
-        //                     <Stack.Screen name={PagesEnum.SignUpPage} component={SignUpPage} options={gestureEnabled} />
-        //                 </Stack.Group>
-        //
-        //                 {/* App content Group */}
-        //                 <Stack.Group screenOptions={{ animation: 'none' }}>
-        //                     <Stack.Screen name={PagesEnum.MealPage} component={MealPage} options={gestureDisabled} />
-        //                     <Stack.Screen name={PagesEnum.ScanPage} component={ScanPage} options={gestureDisabled} />
-        //                     <Stack.Screen name={PagesEnum.HomePage} component={HomePage} options={gestureDisabled} />
-        //                     <Stack.Screen
-        //                         name={PagesEnum.RecipePage}
-        //                         component={RecipePage}
-        //                         options={gestureDisabled}
-        //                     />
-        //                     <Stack.Screen name={PagesEnum.GamePage} component={GamePage} options={gestureDisabled} />
-        //                     <Stack.Screen
-        //                         name={PagesEnum.HistoricalPage}
-        //                         component={HistoricalPage}
-        //                         options={gestureEnabled}
-        //                     />
-        //                 </Stack.Group>
-        //             </Stack.Navigator>
-        //         </NavigationContainer>
-        //     </AppContainer>
-        // </StoreProvider>
+        <StoreProvider value={rootStore}>
+            <AppContainer>
+                <NavigationContainer>
+                    <Stack.Navigator
+                        initialRouteName={PagesEnum.StartUpPage}
+                        screenOptions={{
+                            headerShown: false
+                        }}>
+                        {/* Authentification group */}
+                        <Stack.Group>
+                            <Stack.Screen
+                                name={PagesEnum.StartUpPage}
+                                component={StartUpPage}
+                                options={gestureDisabled}
+                            />
+                            <Stack.Screen name={PagesEnum.LoginPage} component={LoginPage} options={gestureDisabled} />
+                            <Stack.Screen name={PagesEnum.SignUpPage} component={SignUpPage} options={gestureEnabled} />
+                        </Stack.Group>
+
+                        {/* App content Group */}
+                        <Stack.Group screenOptions={{ animation: 'none' }}>
+                            <Stack.Screen name={PagesEnum.MealPage} component={MealPage} options={gestureDisabled} />
+                            <Stack.Screen name={PagesEnum.ScanPage} component={ScanPage} options={gestureDisabled} />
+                            <Stack.Screen name={PagesEnum.HomePage} component={HomePage} options={gestureDisabled} />
+                            <Stack.Screen
+                                name={PagesEnum.RecipePage}
+                                component={RecipePage}
+                                options={gestureDisabled}
+                            />
+                            <Stack.Screen name={PagesEnum.GamePage} component={GamePage} options={gestureDisabled} />
+                            <Stack.Screen
+                                name={PagesEnum.HistoricalPage}
+                                component={HistoricalPage}
+                                options={gestureEnabled}
+                            />
+                        </Stack.Group>
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </AppContainer>
+        </StoreProvider>
     );
 }
 
