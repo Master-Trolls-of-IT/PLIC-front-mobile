5ca539de0c7730bc230a1be57cf02520a8caf71c
Maxner Demilon <124256635+AymanImhah@users.noreply.github.com>
2023-06-08 12:54:43 +0200

    Parsing product nutrients

--- src/infrastructure/ui/pages/scan-page/scan-page-service.ts
+++ src/infrastructure/ui/pages/scan-page/scan-page-service.ts
@@ -7,10 +7,25 @@ const useScanPageService = () => {
         LogStore: { error }
     } = useStore();
 
+    function parsingProductNutrients(obj: { [x: string]: any }, obj100g: { [x: string]: any }) {
+        for (const key in obj) {
+            if (typeof obj[key] === 'object' && obj[key] !== null) {
+                parsingProductNutrients(obj[key], obj100g);
+            } else if (key.endsWith('100g')) {
+                obj100g[key] = obj[key];
+            }
+        }
+    }
     const getProduct = ({ inputBarCode, dispatch }: ScanPageServiceProps) =>
         axios
             .get(process.env.OPENFOODFACTS_API_ENDPOINT + inputBarCode)
             .then((response) => {
+                const obj100g = {};
+                parsingProductNutrients(response.data, obj100g);
+                const {
+                    product: { image_url: image }
+                } = response.data;
+                console.log(obj100g);
                 const {
                     product: { product_name: productName, product_name_fr: productNameFr }
                 } = response.data;
