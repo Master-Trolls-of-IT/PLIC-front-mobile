e75e3ca6d2b53b7c849cfef37e75f5b84050cc86
Mathis <mathismoreau0610@gmail.com>
2023-10-30 18:01:25 +0100

    Fix several warnings in the app

--- src/domain/interfaces/props/widgets/widget-slot-props.ts
+++ src/domain/interfaces/props/widgets/widget-slot-props.ts
@@ -1,5 +1,7 @@
+import { Dispatch, SetStateAction } from 'react';
+
 export type WidgetSlotProps = {
     id: number;
     widgetDropped: { type: 'small' | 'large'; x: number; y: number } | undefined;
-    setHandleDrop?: React.Dispatch<React.SetStateAction<{ isLine: boolean; id: number } | undefined>>;
+    setHandleDrop?: Dispatch<SetStateAction<{ isLine: boolean; id: number } | undefined>>;
 };
