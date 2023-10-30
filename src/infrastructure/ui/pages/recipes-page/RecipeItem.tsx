adf60d5d3c52df6380e2ef13cb692af35f0d49c6
julien gourmet <julien.gourmet@epita.fr>
2023-10-30 20:58:28 +0100

    ui recipeItem

--- src/infrastructure/ui/pages/recipes-page/RecipeItem.tsx
+++ src/infrastructure/ui/pages/recipes-page/RecipeItem.tsx
@@ -1,29 +1,111 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import recipePageStyle from '~/infrastructure/ui/pages/recipes-page/recipe-page-style';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { ColorEnum } from '~/domain/interfaces/enum/color-enum';
 
-const RecipeItem = ({ recipe }) => {
-    return (
-        <View style={recipePageStyle.container}>
-            <Text style={recipePageStyle.title}>{recipe.title}</Text>
-            <Text style={recipePageStyle.ingredients}>Ingrédients: {recipe.ingredients.join(', ')}</Text>
-            {/* Affichez d'autres détails de la recette ici */}
-        </View>
-    );
-};
-StyleSheet.create({
-    container: {
-        borderWidth: 1,
-        borderColor: 'lightgray',
+interface RecipeItemProps {
+    image: string;
+    name: string;
+    rating: number;
+    numRatings: number;
+    prepTime: string;
+    difficulty: string;
+    ecoScore: string;
+    mainIngredients: string[];
+    createdBy: string;
+    tags: string[];
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        width: 351,
+        height: 164,
         padding: 16,
-        marginBottom: 16
+        marginBottom: 16,
+        borderRadius: 20, // Bord arrondi
+        backgroundColor: ColorEnum.ExtraOpaqueBrown
     },
-    title: {
+    container: {
+        flexDirection: 'row' // Disposition en ligne (deux colonnes)
+    },
+    leftContainer: {
+        flex: 1 // Égal partage de l'espace entre les deux conteneurs
+    },
+    rightContainer: {
+        flex: 2, // Égal partage de l'espace entre les deux conteneurs
+        marginLeft: 10 // Marge entre les deux conteneurs
+    },
+    image: {
+        width: 100,
+        height: 100,
+        marginBottom: 10,
+        borderRadius: 15
+    },
+    name: {
         fontSize: 18,
         fontWeight: 'bold'
     },
-    ingredients: {
+    rating: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    prepTime: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+
+    mainIngredients: {
+        fontSize: 16
+    },
+
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    createdBy: {
+        fontSize: 16,
+        textAlign: 'left'
+    },
+    tags: {
         fontSize: 16
     }
 });
+
+const RecipeItem: React.FC<RecipeItemProps> = ({
+    image,
+    name,
+    rating,
+    numRatings,
+    prepTime,
+    difficulty,
+    ecoScore,
+    mainIngredients,
+    createdBy,
+    tags
+}) => {
+    return (
+        <View style={styles.wrapper}>
+            <View style={styles.container}>
+                <View style={styles.leftContainer}>
+                    <Image source={{ uri: image }} style={styles.image} />
+                </View>
+                <View style={styles.rightContainer}>
+                    <Text style={styles.name}>{name}</Text>
+                    <Text style={styles.rating}>
+                        {rating} / 5 ({numRatings} évaluations)
+                    </Text>
+                    <Text style={styles.prepTime}>
+                        {prepTime} {' • '} {difficulty} {' • '} Score : {ecoScore}
+                    </Text>
+                    <Text style={styles.mainIngredients}>{mainIngredients.join(', ')}</Text>
+                </View>
+            </View>
+            <View style={styles.footer}>
+                <Text style={styles.createdBy}>{createdBy}</Text>
+                <Text style={styles.tags}>{tags.join(', ')}</Text>
+            </View>
+        </View>
+    );
+};
+
 export default RecipeItem;
--- src/infrastructure/ui/pages/recipes-page/recipe-page.tsx
+++ src/infrastructure/ui/pages/recipes-page/recipe-page.tsx
@@ -7,6 +7,37 @@ import { ItemEnum } from '~/domain/interfaces/enum/item-enum';
 import GenericButton from '~/infrastructure/ui/shared/component/generic-button/generic-button';
 import useRecipePageData from '~/infrastructure/ui/pages/recipes-page/hooks';
 import GenericHeaderText from '~/infrastructure/ui/shared/component/texts/generic-header-text/generic-header-text';
+import RecipeItem from "~/infrastructure/ui/pages/recipes-page/RecipeItem";
+export const recipeData = [
+    {
+        id: 1,
+        image: 'https://picsum.photos/200/300',
+        title: 'Recette 1',
+        rating: 4.5,
+        numRatings: 50,
+        prepTime: '30min',
+        difficulty: 'Facile',
+        ecoScore: '4.2',
+        mainIngredients: ['Ingrédient 1', 'Ingrédient 2', 'Ingrédient 3'],
+        createdBy: 'Créateur 1',
+        tags: ['Tag 1', 'Tag 2']
+    },
+    {
+        id: 2,
+        image: 'https://picsum.photos/200/300',
+        title: 'Recette 2',
+        rating: 4.0,
+        numRatings: 30,
+        prepTime: '45min',
+        difficulty: 'Moyen',
+        ecoScore: '3.8',
+        mainIngredients: ['Ingrédient 4', 'Ingrédient 5'],
+        createdBy: 'Créateur 2',
+        tags: ['Tag 3', 'Tag 4']
+    },
+    // Ajoutez d'autres recettes simulées ici
+];
+
 const RecipePage = () => {
     const { mealList, onPressCreateRecipe } = useRecipePageData();
 
@@ -22,8 +53,21 @@ const RecipePage = () => {
                     containerStyle={recipePageStyle.headerContainer}
                 />
             </View>
-
-            <SearchList itemType={ItemEnum.Meal} data={mealList} />
+            {recipeData.map((recipe) => (
+                <RecipeItem
+                    key={recipe.id}
+                    name={recipe.title}
+                    rating={recipe.rating}
+                    numRatings={recipe.numRatings}
+                    prepTime={recipe.prepTime}
+                    difficulty={recipe.difficulty}
+                    ecoScore={recipe.ecoScore}
+                    mainIngredients={recipe.mainIngredients}
+                    createdBy={recipe.createdBy}
+                    tags={recipe.tags}
+                    image={recipe.image}
+                />
+            ))}
 
             <GenericButton
                 title={'Créer une recette'}
