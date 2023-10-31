535a37b75c0af4d2419e812486d6a3526ba574e8
Mathis <mathismoreau0610@gmail.com>
2023-10-31 09:36:45 +0100

    change props

--- src/domain/interfaces/props/modal/modal-add-quantity/modal-add-quantity-props.ts
+++ src/domain/interfaces/props/modal/modal-add-quantity/modal-add-quantity-props.ts
@@ -1,10 +1,10 @@
 import { Dispatch, SetStateAction } from 'react';
 
 export type ModalAddQuantityProps = {
-    title?: string;
     modal: boolean;
     setModal: Dispatch<SetStateAction<boolean>> | ((value: boolean) => void);
     addQuantity: (quantity: string) => void | Promise<void>;
+    title?: string;
     isWater?: boolean;
     serving?: number;
     defaultQuantity?: string;
