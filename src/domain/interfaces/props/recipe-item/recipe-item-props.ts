ec93658663a503cc99bae815b26cf9a60279921d
Henry Sargerson <91054494+lesageduvillage@users.noreply.github.com>
2023-11-01 14:45:39 +0100

    another file

--- src/domain/interfaces/props/recipe-item/recipe-item-props.ts
+++ src/domain/interfaces/props/recipe-item/recipe-item-props.ts
@@ -1,4 +1,3 @@
-import RecipeItem from '~/infrastructure/ui/shared/component/recipe-item/recipe-item';
 import { RecipeInfo } from '~/domain/interfaces/props/recipe-item/recipe-item-info';
 
 export type RecipeItemProps = {
