a764ab24be720c0a56df5c1e414fe9cb9ed5e284
Henry Sargerson <91054494+lesageduvillage@users.noreply.github.com>
2023-11-16 15:36:18 +0100

    default recipe data

--- src/infrastructure/controllers/store/root-store/recipe-store.tsx
+++ src/infrastructure/controllers/store/root-store/recipe-store.tsx
@@ -2,51 +2,12 @@ import { action, makeObservable, observable } from 'mobx';
 import { RecipeItemProps } from '~/domain/interfaces/props/search-list/item/recipe-item/recipe-item-props';
 import { RecipeItemInfo } from '~/domain/interfaces/props/search-list/item/recipe-item/recipe-item-info';
 import { ActiveRecipeInfo } from '~/domain/interfaces/props/recipe-item/active-recipe-info';
-
+import { defaultRecipeData } from '~/domain/interfaces/constant/default-recipe-data';
 class RecipeStore {
     recipeList: RecipeItemProps[];
     activeRecipe: RecipeItemInfo | undefined;
     constructor() {
-        this.recipeList = [
-            {
-                style: {},
-                recipeItem: {
-                    id: '1',
-                    title: 'DELICIOUS PASTA',
-                    score: 4.5,
-                    rating: 4.5,
-                    numberOfRatings: 45,
-                    duration: 4,
-                    difficulty: 'tres dur',
-                    ingredients: [
-                        'Pasta',
-                        'Tomato Sauce',
-                        'Cheese',
-                        'test',
-                        'estt',
-                        'klfmd',
-                        'Pasta',
-                        'Tomato Sauce',
-                        'Cheese',
-                        'test',
-                        'estt',
-                        'klfmd'
-                    ],
-                    steps: ['Boil pasta', 'Add tomato sauce', 'Sprinkle cheese', 'Enjoy!', 'test'],
-                    kcal: 350,
-                    image: 'https://resize.elle.fr/original/var/plain_site/storage/images/elle-a-table/les-dossiers-de-la-redaction/dossier-de-la-redac/plat-familial-en-sauce/97666601-1-fre-FR/Un-plat-familial-en-sauce-comme-chez-mamie.jpg',
-                    author: 'Chef John',
-                    isFavourite: true,
-                    tags: [
-                        {
-                            label: 'epicé',
-                            color: 'red'
-                        },
-                        { label: 'sauce', color: 'green' }
-                    ]
-                }
-            }
-        ];
+        this.recipeList = defaultRecipeData;
         this.activeRecipe = undefined;
         makeObservable(
             this,
